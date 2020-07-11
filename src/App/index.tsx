1739a7ab2882e09f3a0958c59d3570583f24b172
Welly Shen <hivoid19@gmail.com>
2020-07-11 18:14:32 +0800

    feat: setup lazy loading pages

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,3 +1,16 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-export default () => <div>React App</div>;
+import Loading from "../components/Loading";
+
+const Home = lazy(() => import("../pages/Home"));
+
+export default () => (
+  <Router>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
