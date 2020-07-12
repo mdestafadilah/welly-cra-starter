3ba0bf403b00315b2f2899769c6016106e3fe88c
Welly Shen <hivoid19@gmail.com>
2020-07-12 14:31:27 +0800

    feat: register pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,7 @@ import { lazy } from "react";
 
 const Login = lazy(() => import("../pages/Login"));
 const Home = lazy(() => import("../pages/Home"));
+const Protected = lazy(() => import("../pages/Protected"));
+const NoMatch = lazy(() => import("../pages/NoMatch"));
 
-export { Login, Home };
+export { Login, Home, Protected, NoMatch };
