787b23c9d31bb20e51d65ba5a2c19bbb846a9369
Welly Shen <hivoid19@gmail.com>
2020-07-13 21:42:07 +0800

    refactor: reuse <Typography />

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,13 +4,14 @@ import { Typography } from "@material-ui/core";
 
 export default (): JSX.Element => {
   const { data, error } = useSWR("/users/1");
+  let txt = "Page loading...";
 
-  if (error) return <Typography variant="h1">Failed to load</Typography>;
-  if (!data) return <Typography variant="h1">Page loading...</Typography>;
+  if (error) txt = "Fail to load data";
+  if (data) txt = data.name;
 
   return (
     <div>
-      <Typography variant="h1">{data.name}</Typography>
+      <Typography variant="h1">{txt}</Typography>
     </div>
   );
 };
