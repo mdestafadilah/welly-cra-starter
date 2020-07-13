bff74ee5ddb631abe1ee6f6b7de5fb722902438f
Welly Shen <hivoid19@gmail.com>
2020-07-13 21:38:45 +0800

    refactor: add refreshInterval config & move fetcher into utils

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import axios from "axios";
 import { SWRConfig } from "swr";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import fetcher from "../utils/fetcher";
 import AppProviders from "../context";
 import Routes from "../pages";
 import { Nav } from "../components";
-import conf from "../configs";
 
 export default (): JSX.Element => (
   <SWRConfig
     value={{
-      fetcher: (url: string): any =>
-        axios(`${conf.API_URL}${url}`).then((res) => res.data),
+      refreshInterval: 3000,
+      fetcher,
     }}
   >
     <AppProviders>
