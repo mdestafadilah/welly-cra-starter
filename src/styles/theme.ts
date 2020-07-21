abef1db43b8eca97750ca0d86790b383457f9293
Welly Shen <hivoid19@gmail.com>
2020-07-21 15:54:14 +0800

    refactor: remove un-necessary overrides

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,5 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 export default createMuiTheme({
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        a: {
-          color: "#fff",
-          textDecoration: "none",
-        },
-      },
-    },
-  },
+  // Customize your theme...
 });
