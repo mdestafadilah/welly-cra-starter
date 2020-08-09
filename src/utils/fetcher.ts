4ee811df368eb4cafd98f5ef400fde03cef96d1d
Welly Shen <hivoid19@gmail.com>
2020-08-09 15:32:10 +0800

    chore: remove un-necessary files

--- src/utils/fetcher.ts
+++
@@ -1,6 +0,0 @@
-import axios, { AxiosPromise } from "axios";
-
-import config from "../config";
-
-export default (url: string): AxiosPromise =>
-  axios(`${config.API_URL}${url}`).then((res) => res.data);
